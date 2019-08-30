7662978bb9a590fc75e0f1786930f99fbfcc0c13
bilihili <luowanshun@gmail.com>
2019-08-30 10:47:45 +0800

    add model topic

--- models.go
+++ models.go
@@ -13,16 +13,24 @@ type Image struct {
 //Article but I want to change it to Post
 type Article struct {
 	gorm.Model
-	Title   string
-	Content string
-	UUID    string
-	UserId  int `gorm:"default:0"`
-	User    User
-	Edited  bool `gorm:"default:false"`
+	Title     string
+	Content   string
+	UUID      string
+	UserId    int `gorm:"default:0"`
+	User      User
+	Edited    bool `gorm:"default:false"`
+	TopicsID  uint
 }
 // User 
 type User struct {
 	gorm.Model
 	UserName string
 	Password string
+}
+// Topics
+type Topics struct {
+	gorm.Model
+	Title        string
+	ImageURL     string
+	Description  string
 }
